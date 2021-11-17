386f531cc2c1b1188da0a113b796fc2ebfb57c7a
HeartyRhombus <kaylahmg@gmail.com>
2021-11-17 10:37:52 -0800

    add solution for centuryFromYear

---
+++ code-signal-solutions.js
@@ -0,0 +1,24 @@
+// centuryFromYear
+function solution(year) {
+        let splitYear = year.toString().split('')
+    if(splitYear.length == 4){
+        let firstTwo = splitYear.slice(0,2).join('')
+        let lastTwo = splitYear.slice(-2).join('')
+        if(lastTwo != '00'){
+            return parseInt(firstTwo)+1
+        } else {
+            return parseInt(firstTwo)
+        }
+    } else if(splitYear.length == 3){
+        let firstNum = splitYear.slice(0,1).join('')
+        let lastTwo = splitYear.slice(-2).join('')
+        console.log()
+        if(lastTwo != '00'){
+            return parseInt(firstNum)+1
+        } else {
+            return parseInt(firstNum)
+        }
+    } else if(splitYear.length <= 2){
+        return 1
+    }
+}
